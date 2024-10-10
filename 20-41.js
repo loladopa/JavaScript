eb32efe5e419b62c020358f29e03c5c74796fd0d
loladopa <loladopa@mail.ru>
2024-10-10 15:07:51 +0300

    javascript

---
+++ 20-41.js
@@ -0,0 +1,92 @@
+let a = 1.5;
+let b = 0.75;
+alert(a+b);
+
+let c = -100;
+alert(c);
+
+let d = 5;
+let f = -d;
+alert(f);
+
+
+let q = 13;
+let qq = 5;
+alert(q%qq);
+
+alert(2 ** 10);
+
+let h = (3 * 2) ** 3;
+alert(h);
+
+
+let str = "Таранец Ярослав";
+alert(str); 
+
+
+let str1 = "!!!";
+alert(str1);
+
+let str2 = "java";
+let str3 = "script";
+alert(str2+str3);
+
+let stra = "hello";
+let strb = " world!";
+alert(stra+strb);
+
+alert('ПРИВЕТ МЕНЯ ЗОВУТ АЛЕЕЕЕЕЕГ'.length); 
+
+let stq = 'xxx';
+let txt = `aaa ${str} bbb`;
+
+let strq = 'xxx';
+let strd = 'yyy';
+let txt1 = `aaa ${strq} bbb ${strd} ccc`;
+alert(txt1);
+
+
+let stro = `abc
+def`; // так будет работать
+alert(stro);
+
+let strf = `a
+b
+c`
+alert(strf);
+
+let ax;
+alert(ax); // выведет undefined
+
+let axx = null;
+alert(axx); // выведет null
+
+let tr = true;
+alert(tr);
+
+let fs = false;
+alert(fs);
+
+
+alert('abs' * 'dcf');
+
+alert( 1 / 0); // выведет Infinity
+alert(-1 / 0); // выведет -Infinity
+
+alert( 10 / 0); // выведет Infinity
+alert(-10 / 0); // выведет -Infinity
+
+let num1 = 1345;
+console.log(num1);
+
+let num2 = 111;
+let num3 = 222;
+let num4 = 333;
+console.log(num2, num3, num4);
+
+console.log(123);
+console.log('123');
+
+alert(eee); // Uncaught ReferenceError: eee is not defined\
+
+const PI = 3.14;
---
+++ script1-20.js
@@ -0,0 +1,70 @@
+"use strict";
+alert('text!');
+
+alert('Привет, мир!'); // комментарий
+
+/*
+	комментарий
+*/
+alert('Привет, мир!');
+
+let num = 123;
+alert(num);
+
+
+let a, b, c; // объявляем все 3 переменные
+
+let d = 1;
+let e = 2;
+let v = 3;
+
+let r = 1, fb = 2, cq = 3;
+
+// Присваиваем переменным значения:
+a = 1;
+b = 2;
+c = 3;
+
+let h;    // объявляем переменную
+
+h = 10;    // записываем в нее значение 1
+alert(h); // выведет 1
+
+h = 20;    // записываем теперь значение 2, затирая значение 1
+alert(h); // выведет 2
+
+let k = 1;
+alert(k);
+
+let l = 2;
+alert(l);
+
+
+let i = 1 + 2 + 3;
+alert(i);
+
+
+
+let aa = 10;
+let ba = 2;
+alert(aa - ba);
+alert(aa + ba);
+alert(aa / ba);
+alert(aa * ba);
+
+let dd = 10;
+let ff = 5;
+alert(dd+ff);
+
+
+let ss = 1;
+let ddd = 2;
+let oo = 3;
+alert(ss+ddd+oo);
+
+let g = 10;
+let j = 5;
+let m = g-j;
+let o = 7;
+alert(m+o);
+
