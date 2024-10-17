23789d6e29d044fc80c4fb3006deec42fcf3bf32
loladopa <loladopa@mail.ru>
2024-10-17 09:05:51 +0300

    2 commit

---
+++ 68-76.js
@@ -0,0 +1,66 @@
+let arrs = [1, 2, 3];
+console.log(arrs);
+alert(arrs);
+let arr = ['a', 'b', 'c'];
+console.log(arr);
+
+
+
+
+let arr1 = [1, 2, 3];
+console.log(arr1[0]); 
+console.log(arr1[1]); 
+console.log(arr1[2]); 
+console.log(Number(arr1[0])+Number(arr1[1])+Number(arr1[2]))
+let arrr = ['a', 'b', 'c', 'd'];
+console.log(arrr[0]+"+"+arrr[1]+"+"+arrr[2]+"+"+arrr[3]);
+
+
+console.log(arr.length)
+
+console.log(arr[arr.length - 1])
+
+
+let arra = ['a', 'b', 'c'];
+
+arra[0] = '1';
+arra[1] = '2';
+arra[2] = '3';
+alert(arra);
+
+let arraa = ['a', 'b', 'c'];
+
+arraa[0] += '1';
+arraa[1] += '2';
+arraa[2] += '3';
+
+console.log(arraa);
+
+let arrd = [1, 2, 3, 4];
+
+arrd[0]++;
+arrd[1]++;
+arrd[2]++;
+arrd[3]++;
+
+console.log(arrd);
+
+
+let arr2 = [];
+
+arr2[0] = '1';
+arr2[1] = '2';
+arr2[2] = '3';
+
+console.log(arr2);
+
+arr2[3] = '4';
+arr2[4] = '5';
+console.log(arr2);
+
+
+let arras = [];
+
+arras[3] = 'a';
+arras[8] = 'b';
+console.log(arras.length);
